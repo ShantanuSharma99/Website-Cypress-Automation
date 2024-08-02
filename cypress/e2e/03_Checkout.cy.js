c0322154c5a20de9fed7f1c2bb1c9b31aac501db
Shantanu Sharma <shantanusharma2015@gmail.com>
2024-08-02 18:31:37 +0530

    first commit

--- cypress/e2e/03_Checkout.cy.js
+++ cypress/e2e/03_Checkout.cy.js
@@ -7,7 +7,7 @@ describe('Login',()=>{
         cy.visit('https://www.automationexercise.com/')
 
         cy.get('.shop-menu > .nav > :nth-child(4) > a').click();
-        cy.get('[data-qa="login-email"]').type('ramkumar2@mailinator.com');
+        cy.get('[data-qa="login-email"]').type('rm@mailinator.com');
         cy.get('[data-qa="login-password"]').type('1234567890');
         cy.get('[data-qa="login-button"]').click();
 
